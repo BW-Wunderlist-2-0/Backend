a03701f7739cb932b486e6fcf5a8331b7d2ace9d
Robert Benavidez <murabitbenavidez@gmail.com>
2019-10-19 10:45:03 -0400

    users table

---
+++ data/migrations/20191019104158_bootstrap.js
@@ -0,0 +1,15 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('users', tbl => {
+      tbl.increments();
+      tbl
+        .string('username', 25)
+        .notNullable()
+        .unique();
+      tbl.string('password', 100)
+        .notNullable();
+    });
+  };
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('users');
+  };
+  

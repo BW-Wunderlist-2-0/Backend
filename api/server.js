5ba104af007fd959365f68cbd8c04f627d8a98d2
Robert Benavidez <murabitbenavidez@gmail.com>
2019-10-21 14:26:34 -0400

    registration and login working and producing jwts

--- api/server.js
+++ api/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const helmet = require('helmet');
+
+const authenticate = require('../auth/authenticate-middleware.js');
+const authRouter = require('../auth/auth-router.js');
 
 const server = express();
 
-server.use(helmet);
+
+//middleware
+server.use(helmet());
+server.use(cors());
 server.use(express.json());
+
+
+// Routes
+server.use('/api/auth', authRouter);
 
 
 
